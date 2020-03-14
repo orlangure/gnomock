c6593a63bec6a04dc10d9389f236592d8fef2e04
Yury Fedorov <orlangure@gmail.com>
2020-03-14 23:44:33 +0200

    Improve test example

--- splunk_test.go
+++ splunk_test.go
@@ -2,6 +2,7 @@ package splunk_test
 
 import (
 	"fmt"
+	"sort"
 
 	"github.com/orlangure/gnomock"
 	mocksplunk "github.com/orlangure/gnomock-splunk"
@@ -31,12 +32,30 @@ func ExampleSplunk() {
 
 	// created container now includes two events in "events" index
 	container, err := gnomock.StartPreset(p)
-	fmt.Println(len(container.Ports), err)
+	fmt.Println("error:", err)
+	fmt.Println(len(container.Ports), "exposed ports:")
 
 	defer func() {
 		_ = gnomock.Stop(container)
 	}()
 
+	// Port numbers as well as container address are non-deterministic, so they
+	// are skipped in this example. The usage would be:
+	//
+	//		container.Address("web")
+	// 		container.Address("api")
+
+	portNames := make([]string, 0)
+
+	for portName := range container.Ports {
+		portNames = append(portNames, portName)
+	}
+
+	sort.Sort(sort.StringSlice(portNames))
+	fmt.Println(portNames)
+
 	// Output:
-	// 3 <nil>
+	// error: <nil>
+	// 3 exposed ports:
+	// [api collector web]
 }
