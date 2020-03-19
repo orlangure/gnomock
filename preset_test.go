c0884cfd3d648fde9bfb0d34c8f62e23552a3a8c
Yury Fedorov <orlangure@gmail.com>
2020-03-19 22:10:15 +0200

    Allow to override Preset options

---
+++ preset_test.go
@@ -0,0 +1,62 @@
+package gnomock_test
+
+import (
+	"fmt"
+	"testing"
+
+	"github.com/orlangure/gnomock"
+	"github.com/stretchr/testify/require"
+)
+
+func TestPreset(t *testing.T) {
+	t.Parallel()
+
+	p := &testPreset{}
+	container, err := gnomock.StartPreset(p)
+
+	defer func(c *gnomock.Container) {
+		require.NoError(t, gnomock.Stop(c))
+	}(container)
+
+	// by default, testPreset always fails its healthcheck
+	require.Error(t, err)
+
+	container, err = gnomock.StartPreset(p, gnomock.WithHealthCheck(healthcheck))
+
+	defer func(c *gnomock.Container) {
+		require.NoError(t, gnomock.Stop(c))
+	}(container)
+
+	// if we override healthcheck, container start correctly
+	require.NoError(t, err)
+}
+
+type testPreset struct{}
+
+// Image returns a canonical docker image used to setup this Preset
+func (t *testPreset) Image() string {
+	return testImage
+}
+
+// Ports returns a group of ports exposed by this Preset, where every port
+// is given a unique name.
+//
+// This Preset returns port 80 by "default" name
+func (t *testPreset) Ports() gnomock.NamedPorts {
+	namedPorts := gnomock.NamedPorts{
+		"web80":   gnomock.TCP(goodPort80),
+		"web8080": gnomock.TCP(goodPort8080),
+	}
+
+	return namedPorts
+}
+
+// Options returns a list of Option functions that allow to setup this
+// Preset implementation. This test preset always returns a failing healthcheck
+func (t *testPreset) Options() []gnomock.Option {
+	return []gnomock.Option{
+		gnomock.WithHealthCheck(func(*gnomock.Container) error {
+			return fmt.Errorf("this container should not start")
+		}),
+	}
+}
