2dbb43b550d3234e6a3c42ab4edae4c43a32f0e2
Yury Fedorov <orlangure@gmail.com>
2020-05-02 01:27:55 +0300

    Add `-v` flag for current version info
    To set the version, use:
    
    ```
    go build -o app -ldflags "-X main.version=v0.1.0"
    ```

--- main.go
+++ main.go
@@ -5,15 +5,27 @@ import (
 	"fmt"
 	"log"
 	"net/http"
+	"os"
 
 	"github.com/orlangure/gnomockd/gnomockd"
 )
 
-func main() {
-	var port int
+var version string
 
+func main() {
+	var (
+		v    bool
+		port int
+	)
+
+	flag.BoolVar(&v, "v", false, "display current version")
 	flag.IntVar(&port, "port", 23042, "gnomockd port number")
 	flag.Parse()
+
+	if v {
+		fmt.Println(version)
+		os.Exit(0)
+	}
 
 	addr := fmt.Sprintf("127.0.0.1:%d", port)
 	log.Println(http.ListenAndServe(addr, gnomockd.Handler()))
