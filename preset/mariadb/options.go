7f976aa11dc82812ee6db8c3b9880803bddcc718
Dušan Simić <dusan.simic1810@gmail.com>
2020-10-02 23:27:39 +0200

    Add MariaDB preset

---
+++ preset/mariadb/options.go
@@ -0,0 +1,46 @@
+package mariadb
+
+// Option is an optional configuration of this Gnomock preset. Use available
+// Options to configure the container
+type Option func(*P)
+
+// WithUser creates a new superuser with the provided credentials in the
+// container. If not used, the default credentials are gnomock:gnoria
+func WithUser(user, password string) Option {
+	return func(p *P) {
+		p.User = user
+		p.Password = password
+	}
+}
+
+// WithDatabase creates a database with the provided name in the container. If
+// not provided, "mydb" is used by default.  WithQueries, if provided, runs
+// against the new database
+func WithDatabase(db string) Option {
+	return func(p *P) {
+		p.DB = db
+	}
+}
+
+// WithQueries executes the provided queries against the database created with
+// WithDatabase, or against default "mydb" database
+func WithQueries(queries ...string) Option {
+	return func(p *P) {
+		p.Queries = append(p.Queries, queries...)
+	}
+}
+
+// WithQueriesFile sets a file name to read initial queries from. Queries from
+// this file are executed before any other queries provided in WithQueries
+func WithQueriesFile(file string) Option {
+	return func(p *P) {
+		p.QueriesFiles = append(p.QueriesFiles, file)
+	}
+}
+
+// WithVersion sets image version.
+func WithVersion(version string) Option {
+	return func(o *P) {
+		o.Version = version
+	}
+}
