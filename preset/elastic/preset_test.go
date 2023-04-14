8406e8abef2d274c84ee7829cbe8752647da0795
Marvin Zeising <marvin.zeising@staffbase.com>
2023-04-14 14:56:38 +0200

    simplify preset test to only test elasticsearch 8.7.0

--- preset/elastic/preset_test.go
+++ preset/elastic/preset_test.go
@@ -20,62 +20,54 @@ func TestPreset(t *testing.T) {
 		t.Skip("elastic tests can't run with race detector due to https://github.com/elastic/go-elasticsearch/issues/147")
 	}
 
-	for _, version := range []string{"8.7.0", "7.9.3", "6.8.13", "5.6"} {
-		t.Run(version, testPreset(version))
+	p := elastic.Preset(
+		elastic.WithVersion("8.7.0"),
+		elastic.WithInputFile("./testdata/titles"),
+		elastic.WithInputFile("./testdata/names"),
+	)
+
+	c, err := gnomock.Start(p)
+	require.NoError(t, err)
+
+	defer func() { require.NoError(t, gnomock.Stop(c)) }()
+
+	cfg := elasticsearch.Config{
+		Addresses:    []string{fmt.Sprintf("http://%s", c.DefaultAddress())},
+		DisableRetry: true,
 	}
-}
 
-func testPreset(version string) func(t *testing.T) {
-	return func(t *testing.T) {
-		p := elastic.Preset(
-			elastic.WithVersion(version),
-			elastic.WithInputFile("./testdata/titles"),
-			elastic.WithInputFile("./testdata/names"),
-		)
+	client, err := elasticsearch.NewClient(cfg)
+	require.NoError(t, err)
 
-		c, err := gnomock.Start(p)
-		require.NoError(t, err)
+	res, err := client.Search(
+		client.Search.WithIndex("titles"),
+		client.Search.WithQuery("gnomock"),
+	)
+	require.NoError(t, err)
+	require.False(t, res.IsError(), res.String())
 
-		defer func() { require.NoError(t, gnomock.Stop(c)) }()
-
-		cfg := elasticsearch.Config{
-			Addresses:    []string{fmt.Sprintf("http://%s", c.DefaultAddress())},
-			DisableRetry: true,
-		}
-
-		client, err := elasticsearch.NewClient(cfg)
-		require.NoError(t, err)
-
-		res, err := client.Search(
-			client.Search.WithIndex("titles"),
-			client.Search.WithQuery("gnomock"),
-		)
-		require.NoError(t, err)
-		require.False(t, res.IsError(), res.String())
-
-		var out struct {
-			Hits struct {
-				Total struct {
-					Value int `json:"value"`
-				} `json:"total"`
-			} `json:"hits"`
-		}
-
-		require.NoError(t, json.NewDecoder(res.Body).Decode(&out))
-		require.NoError(t, res.Body.Close())
-		require.Equal(t, 1, out.Hits.Total.Value)
-
-		res, err = client.Search(
-			client.Search.WithIndex("titles"),
-			client.Search.WithQuery("unknown"),
-		)
-		require.NoError(t, err)
-		require.False(t, res.IsError(), res.String())
-
-		require.NoError(t, json.NewDecoder(res.Body).Decode(&out))
-		require.NoError(t, res.Body.Close())
-		require.Equal(t, 0, out.Hits.Total.Value)
+	var out struct {
+		Hits struct {
+			Total struct {
+				Value int `json:"value"`
+			} `json:"total"`
+		} `json:"hits"`
 	}
+
+	require.NoError(t, json.NewDecoder(res.Body).Decode(&out))
+	require.NoError(t, res.Body.Close())
+	require.Equal(t, 1, out.Hits.Total.Value)
+
+	res, err = client.Search(
+		client.Search.WithIndex("titles"),
+		client.Search.WithQuery("unknown"),
+	)
+	require.NoError(t, err)
+	require.False(t, res.IsError(), res.String())
+
+	require.NoError(t, json.NewDecoder(res.Body).Decode(&out))
+	require.NoError(t, res.Body.Close())
+	require.Equal(t, 0, out.Hits.Total.Value)
 }
 
 func TestPreset_withDefaults(t *testing.T) {
