1429b716e637b1cebf7992966ee2bc957b72683b
D061920 <johannes.vietze@sap.com>
2024-06-20 10:35:56 +0200

    remove comment

--- internal/gnomockd/localstack_test.go
+++ internal/gnomockd/localstack_test.go
@@ -50,7 +50,6 @@ func TestLocalstack(t *testing.T) {
 
 	cfg, err := config.LoadDefaultConfig(context.TODO(),
 		config.WithRegion("us-east-1"),
-		//config.WithEC2IMDSEndpoint(s3Endpoint),
 		config.WithCredentialsProvider(aws.CredentialsProviderFunc(func(ctx context.Context) (aws.Credentials, error) {
 			return aws.Credentials{
 				AccessKeyID:     "a",
